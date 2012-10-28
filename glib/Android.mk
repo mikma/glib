389a6de055723b930c6e21af5f25968c63798636
Mikael Magnusson <mikma@users.sourceforge.net>
2012-10-28 22:41:25 +0100

    Android: support static and shared glib module using ndk-build

--- glib/Android.mk
+++ glib/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH:= $(call my-dir)
 
-include $(CLEAR_VARS)
+GLIB_TOP := $(LOCAL_PATH)/..
 
-LOCAL_SRC_FILES:= \
+local_src_files:= \
     ./libcharset/localcharset.c \
     garray.c        \
     gasyncqueue.c   \
@@ -74,9 +74,7 @@ LOCAL_SRC_FILES:= \
     giounix.c       \
     gspawn.c
 
-LOCAL_MODULE := libglib-2.0
-
-LOCAL_C_INCLUDES := 			\
+local_c_includes := 			\
 	$(GLIB_TOP)			\
 	$(GLIB_TOP)/android		\
 	$(GLIB_TOP)/android-internal	\
@@ -86,7 +84,7 @@ LOCAL_C_INCLUDES := 			\
 	$(LOCAL_PATH)/pcre
 
 # ./glib private macros, copy from Makefile.am
-LOCAL_CFLAGS := \
+local_cflags := \
 	-DLIBDIR=\"$(libdir)\"		\
 	-DHAVE_CONFIG_H			\
 	-DG_LOG_DOMAIN=\"GLib\"		\
@@ -94,14 +92,41 @@ LOCAL_CFLAGS := \
 	-DG_DISABLE_DEPRECATED		\
 	-DGLIB_COMPILATION
 
-ifeq ($(GLIB_BUILD_STATIC),true)
+local_export_c_includes := \
+	$(LOCAL_PATH) \
+	$(LOCAL_PATH)/.. \
+	$(LOCAL_PATH)/../android
+
+#
+# Static library
+#
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libglib-2.0_static
+LOCAL_CFLAGS := $(local_cflags)
+LOCAL_C_INCLUDES := $(local_c_includes)
+LOCAL_SRC_FILES := $(local_src_files)
+
+LOCAL_EXPORT_C_INCLUDES += $(local_export_c_includes)
+
 include $(BUILD_STATIC_LIBRARY)
-else
+
+#
+# Shared library
+#
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libglib-2.0
+LOCAL_CFLAGS := $(local_cflags)
+LOCAL_C_INCLUDES := $(local_c_includes)
+LOCAL_SRC_FILES := $(local_src_files)
+
+LOCAL_EXPORT_C_INCLUDES += $(local_export_c_includes)
+
 LOCAL_STATIC_LIBRARIES := libpcre
 LOCAL_LDLIBS :=				\
 	-llog
 
 include $(BUILD_SHARED_LIBRARY)
-endif
 
 include $(LOCAL_PATH)/pcre/Android.mk
